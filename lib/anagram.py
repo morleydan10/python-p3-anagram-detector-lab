f21532521b6b87be0284045d7c8c842f795b7a33
Daniel Morley <morleydan10@gmail.com>
2023-12-16 19:12:22 -0500

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,31 @@
 # your code goes here!
+# class Anagram:
+#     def __init__(self, word) -> None:
+#         self.word = word
+    
+#     @property
+#     def word(self):
+#         return self._word
+    
+#     @word.setter
+#     def word(self, word):
+#         pass
+
+#     def match(self):
+        
+#         pass
+
+#     pass
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list
